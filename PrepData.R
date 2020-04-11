4c3b949f310c813bd03e47c14eff20eaa3452fa8
Tai Nguyen <tainguyen7597@gmail.com>
2020-04-11 17:53:34 -0400

    chart works now

--- PrepData.R
+++ PrepData.R
@@ -1,12 +1,20 @@
-library(data.table)
+library(dplyr)
 
 # Make sure to set Working Directory
 df <- read.csv('./data/NationalNames.csv')
-# Post 1960
-df <- df[df$Year > 1960,]
-# Female
-df_f <- df[df$Gender == 'F',]
-df_f <- df_f[with(df_f, order(Name,-Year)),] 
-row.names(df_f) <- NULL #reset index
+# Post 1975
+df <- df[df$Year >= 1975,]
+df <- df[with(df, order(Name,-Year)),] 
+row.names(df) <- NULL #reset index
 
-data
+df <-
+  df %>%
+  group_by(Name) %>%
+  mutate(lastCount = dplyr::lag(Count, n=1, default=NA))
+
+df <-
+  df %>%
+  group_by(Year) %>%
+  mutate(rank = order(order(Count, Year, decreasing=TRUE)))
+
+write.csv(df,'./data/clean-national-names.csv', row.names = FALSE)
--- race-bar.js
+++ race-bar.js
@@ -1,260 +1,261 @@
- var svg = d3.select("body").append("svg")
-   .attr("width", 960)
-   .attr("height", 600);
+var svg = d3.select("body").append("svg")
+  .attr("width", 960)
+  .attr("height", 600);
 
- var tickDuration = 500;
+var tickDuration = 500;
 
- var top_n = 12;
- var height = 600;
- var width = 960;
+var top_n = 12;
+var height = 600;
+var width = 960;
 
- const margin = {
-   top: 80,
-   right: 0,
-   bottom: 5,
-   left: 0
- };
+const margin = {
+  top: 80,
+  right: 0,
+  bottom: 5,
+  left: 0
+};
 
- let barPadding = (height-(margin.bottom+margin.top))/(top_n*5);
+let barPadding = (height-(margin.bottom+margin.top))/(top_n*5);
 
- let title = svg.append('text')
-  .attr('class', 'title')
-  .attr('y', 24)
-  .html('18 years of Interbrand’s Top Global Brands');
+let title = svg.append('text')
+ .attr('class', 'title')
+ .attr('y', 24)
+ .html('18 years of Interbrand’s Top Global Brands');
 
- let subTitle = svg.append("text")
-  .attr("class", "subTitle")
-  .attr("y", 55)
-  .html("Brand value, $m");
+let subTitle = svg.append("text")
+ .attr("class", "subTitle")
+ .attr("y", 55)
+ .html("Brand value, $m");
 
- let caption = svg.append('text')
-  .attr('class', 'caption')
-  .attr('x', width)
-  .attr('y', height-5)
-  .style('text-anchor', 'end')
-  .html('Source: Interbrand');
+let caption = svg.append('text')
+ .attr('class', 'caption')
+ .attr('x', width)
+ .attr('y', height-5)
+ .style('text-anchor', 'end')
+ .html('Source: Interbrand');
 
-  let year = 2006;
+ let year = 1975;
 
-d3.csv('/data/IMDB-Movie-Data.csv').then(function(data) {
- //if (error) throw error;
-
-   console.log(data);
-
-    data.forEach(d => {
-     d.name = d.title,
-     d.value = +d.revenue,
-     d.lastValue = +d.revenue - (0.1*+d.revenue),
-     d.value = isNaN(d.revenue) ? 0 : d.revenue,
-     d.year = +d.year,
-     d.colour = d3.hsl(Math.random()*360,0.75,0.75)
-   });
+d3.csv('/data/clean-national-names.csv').then(function(data) {
+//if (error) throw error;
 
   console.log(data);
 
-  let yearSlice = data.filter(d => +d.year == year && !isNaN(+d.value))
-   .sort((a,b) => +b.value - +a.value)
-   .slice(0, top_n);
+   data.forEach(d => {
+    d.name = d.Name,
+    d.value = +d.Count,
+    d.lastValue = +d.lastCount,
+    d.value = isNaN(+d.Count) ? 0 : +d.Count,
+    d.year = d.Year,
+    d.colour = d3.hsl(Math.random()*360,0.75,0.75),
+    d.rank = +d.rank
+  });
 
-   yearSlice.forEach((d,i) => d.rank = i);
+ console.log(data);
 
-  console.log('yearSlice: ', yearSlice)
+ let yearSlice = data.filter(d => +d.year == year && !isNaN(+d.value))
+  .sort((a,b) => +b.value - +a.value)
+  .slice(0, top_n);
 
-  let x = d3.scaleLinear()
-     .domain([0, d3.max(yearSlice, d => +d.value)])
-     .range([margin.left, width-margin.right-65]);
+  yearSlice.forEach((d,i) => d.rank = i);
 
-  let y = d3.scaleLinear()
-     .domain([top_n, 0])
-     .range([height-margin.bottom, margin.top]);
+ console.log('yearSlice: ', yearSlice)
 
-  let xAxis = d3.axisTop()
-     .scale(x)
-     .ticks(width > 500 ? 5:2)
-     .tickSize(-(height-margin.top-margin.bottom))
-     .tickFormat(d => d3.format(',')(d));
+ let x = d3.scaleLinear()
+    .domain([0, d3.max(yearSlice, d => +d.value)])
+    .range([margin.left, width-margin.right-65]);
 
-  svg.append('g')
-    .attr('class', 'axis xAxis')
-    .attr('transform', `translate(0, ${margin.top})`)
-    .call(xAxis)
-    .selectAll('.tick line')
-    .classed('origin', d => d == 0);
+ let y = d3.scaleLinear()
+    .domain([top_n, 0])
+    .range([height-margin.bottom, margin.top]);
 
-  svg.selectAll('rect.bar')
-     .data(yearSlice, d => d.name)
-     .enter()
-     .append('rect')
-     .attr('class', 'bar')
-     .attr('x', x(0)+1)
-     .attr('width', d => x(+d.value)-x(0)-1)
-     .attr('y', d => y(+d.rank)+5)
-     .attr('height', y(1)-y(0)-barPadding)
-     .style('fill', d => d.colour);
+ let xAxis = d3.axisTop()
+    .scale(x)
+    .ticks(width > 500 ? 5:2)
+    .tickSize(-(height-margin.top-margin.bottom))
+    .tickFormat(d => d3.format(',')(d));
 
-  svg.selectAll('text.label')
-     .data(yearSlice, d => d.name)
-     .enter()
-     .append('text')
-     .attr('class', 'label')
-     .attr('x', d => x(+d.value)-8)
-     .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1)
-     .style('text-anchor', 'end')
-     .html(d => d.name);
+ svg.append('g')
+   .attr('class', 'axis xAxis')
+   .attr('transform', `translate(0, ${margin.top})`)
+   .call(xAxis)
+   .selectAll('.tick line')
+   .classed('origin', d => d == 0);
 
- svg.selectAll('text.valueLabel')
-   .data(yearSlice, d => d.name)
-   .enter()
-   .append('text')
-   .attr('class', 'valueLabel')
-   .attr('x', d => x(+d.value)+5)
-   .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1)
-   .text(d => d3.format(',.0f')(d.lastValue));
+ svg.selectAll('rect.bar')
+    .data(yearSlice, d => d.name)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', x(0)+1)
+    .attr('width', d => x(+d.value)-x(0)-1)
+    .attr('y', d => y(+d.rank)+5)
+    .attr('height', y(1)-y(0)-barPadding)
+    .style('fill', d => d.colour);
 
- let yearText = svg.append('text')
-   .attr('class', 'yearText')
-   .attr('x', width-margin.right)
-   .attr('y', height-25)
-   .style('text-anchor', 'end')
-   .html(~~year)
-   .call(halo, 10);
+ svg.selectAll('text.label')
+    .data(yearSlice, d => d.name)
+    .enter()
+    .append('text')
+    .attr('class', 'label')
+    .attr('x', d => x(d.value)-8)
+    .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1)
+    .style('text-anchor', 'end')
+    .html(d => d.name);
+
+svg.selectAll('text.valueLabel')
+  .data(yearSlice, d => d.name)
+  .enter()
+  .append('text')
+  .attr('class', 'valueLabel')
+  .attr('x', d => x(d.value)+5)
+  .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1)
+  .text(d => d3.format(',.0f')(+d.lastValue));
+
+let yearText = svg.append('text')
+  .attr('class', 'yearText')
+  .attr('x', width-margin.right)
+  .attr('y', height-25)
+  .style('text-anchor', 'end')
+  .html(~~year)
+  .call(halo, 10);
 
 let ticker = d3.interval(e => {
 
-   yearSlice = data.filter(d => d.year == year && !isNaN(+d.value))
-     .sort((a,b) => +b.value - +a.value)
-     .slice(0,top_n);
+  yearSlice = data.filter(d => d.year == year && !isNaN(+d.value))
+    .sort((a,b) => +b.value - +a.value)
+    .slice(0,top_n);
 
-   yearSlice.forEach((d,i) => d.rank = i);
+  yearSlice.forEach((d,i) => d.rank = i);
 
-   // console.log('IntervalYear: ', yearSlice);
+  // console.log('IntervalYear: ', yearSlice);
 
-   x.domain([0, d3.max(yearSlice, d => +d.value)]);
+  x.domain([0, d3.max(yearSlice, d => d.value)]);
 
-   svg.select('.xAxis')
-     .transition()
-     .duration(tickDuration)
-     .ease(d3.easeLinear)
-     .call(xAxis);
+  svg.select('.xAxis')
+    .transition()
+    .duration(tickDuration)
+    .ease(d3.easeLinear)
+    .call(xAxis);
 
-    let bars = svg.selectAll('.bar').data(yearSlice, d => d.name);
+   let bars = svg.selectAll('.bar').data(yearSlice, d => d.name);
 
-    bars
-     .enter()
-     .append('rect')
-     .attr('class', d => `bar ${d.name.replace(/\s/g,'_')}`)
-     .attr('x', x(0)+1)
-     .attr( 'width', d => x(+d.value)-x(0)-1)
-     .attr('y', d => y(top_n+1)+5)
-     .attr('height', y(1)-y(0)-barPadding)
-     .style('fill', d => d.colour)
-     .transition()
-       .duration(tickDuration)
-       .ease(d3.easeLinear)
-       .attr('y', d => y(+d.rank)+5);
+   bars
+    .enter()
+    .append('rect')
+    .attr('class', d => `bar ${d.name.replace(/\s/g,'_')}`)
+    .attr('x', x(0)+1)
+    .attr( 'width', d => x(d.value)-x(0)-1)
+    .attr('y', d => y(top_n+1)+5)
+    .attr('height', y(1)-y(0)-barPadding)
+    .style('fill', d => d.colour)
+    .transition()
+      .duration(tickDuration)
+      .ease(d3.easeLinear)
+      .attr('y', d => y(+d.rank)+5);
 
-    bars
-     .transition()
-       .duration(tickDuration)
-       .ease(d3.easeLinear)
-       .attr('width', d => x(+d.value)-x(0)-1)
-       .attr('y', d => y(+d.rank)+5);
+   bars
+    .transition()
+      .duration(tickDuration)
+      .ease(d3.easeLinear)
+      .attr('width', d => x(+d.value)-x(0)-1)
+      .attr('y', d => y(+d.rank)+5);
 
-    bars
-     .exit()
-     .transition()
-       .duration(tickDuration)
-       .ease(d3.easeLinear)
-       .attr('width', d => x(+d.value)-x(0)-1)
-       .attr('y', d => y(top_n+1)+5)
-       .remove();
+   bars
+    .exit()
+    .transition()
+      .duration(tickDuration)
+      .ease(d3.easeLinear)
+      .attr('width', d => x(+d.value)-x(0)-1)
+      .attr('y', d => y(top_n+1)+5)
+      .remove();
 
-    let labels = svg.selectAll('.label')
-       .data(yearSlice, d => d.name);
+   let labels = svg.selectAll('.label')
+      .data(yearSlice, d => d.name);
 
-    labels
-     .enter()
-     .append('text')
-     .attr('class', 'label')
-     .attr('x', d => x(+d.value)-8)
-     .attr('y', d => y(top_n+1)+5+((y(1)-y(0))/2))
-     .style('text-anchor', 'end')
-     .html(d => d.name)
-     .transition()
-       .duration(tickDuration)
-       .ease(d3.easeLinear)
-       .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1);
-
-
-    labels
-       .transition()
-       .duration(tickDuration)
-         .ease(d3.easeLinear)
-         .attr('x', d => x(+d.value)-8)
-         .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1);
-
-    labels
-       .exit()
-       .transition()
-         .duration(tickDuration)
-         .ease(d3.easeLinear)
-         .attr('x', d => x(+d.value)-8)
-         .attr('y', d => y(top_n+1)+5)
-         .remove();
+   labels
+    .enter()
+    .append('text')
+    .attr('class', 'label')
+    .attr('x', d => x(+d.value)-8)
+    .attr('y', d => y(top_n+1)+5+((y(1)-y(0))/2))
+    .style('text-anchor', 'end')
+    .html(d => d.name)
+    .transition()
+      .duration(tickDuration)
+      .ease(d3.easeLinear)
+      .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1);
 
 
+   labels
+      .transition()
+      .duration(tickDuration)
+        .ease(d3.easeLinear)
+        .attr('x', d => x(+d.value)-8)
+        .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1);
 
-    let valueLabels = svg.selectAll('.valueLabel').data(yearSlice, d => d.name);
+   labels
+      .exit()
+      .transition()
+        .duration(tickDuration)
+        .ease(d3.easeLinear)
+        .attr('x', d => x(+d.value)-8)
+        .attr('y', d => y(top_n+1)+5)
+        .remove();
 
-    valueLabels
-       .enter()
-       .append('text')
-       .attr('class', 'valueLabel')
-       .attr('x', d => x(+d.value)+5)
-       .attr('y', d => y(top_n+1)+5)
-       .text(d => d3.format(',.0f')(+d.lastValue))
-       .transition()
-         .duration(tickDuration)
-         .ease(d3.easeLinear)
-         .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1);
 
-    valueLabels
-       .transition()
-         .duration(tickDuration)
-         .ease(d3.easeLinear)
-         .attr('x', d => x(+d.value)+5)
-         .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1)
-         .tween("text", function(d) {
-            let i = d3.interpolateRound(+d.lastValue, +d.value);
-            return function(t) {
-              this.textContent = d3.format(',')(i(t));
-           };
-         });
 
+   let valueLabels = svg.selectAll('.valueLabel').data(yearSlice, d => d.name);
 
    valueLabels
-     .exit()
-     .transition()
-       .duration(tickDuration)
-       .ease(d3.easeLinear)
-       .attr('x', d => x(+d.value)+5)
-       .attr('y', d => y(top_n+1)+5)
-       .remove();
+      .enter()
+      .append('text')
+      .attr('class', 'valueLabel')
+      .attr('x', d => x(+d.value)+5)
+      .attr('y', d => y(top_n+1)+5)
+      .text(d => d3.format(',.0f')(+d.lastValue))
+      .transition()
+        .duration(tickDuration)
+        .ease(d3.easeLinear)
+        .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1);
 
-   yearText.html(~~year);
+   valueLabels
+      .transition()
+        .duration(tickDuration)
+        .ease(d3.easeLinear)
+        .attr('x', d => x(+d.value)+5)
+        .attr('y', d => y(+d.rank)+5+((y(1)-y(0))/2)+1)
+        .tween("text", function(d) {
+           let i = d3.interpolateRound(+d.lastValue, +d.value);
+           return function(t) {
+             this.textContent = d3.format(',')(i(t));
+          };
+        });
 
-  if(year == 2016) ticker.stop();
-  year = d3.format('.1f')((+year) + 0.1);
+
+  valueLabels
+    .exit()
+    .transition()
+      .duration(tickDuration)
+      .ease(d3.easeLinear)
+      .attr('x', d => x(+d.value)+5)
+      .attr('y', d => y(top_n+1)+5)
+      .remove();
+
+  yearText.html(~~year);
+
+ if(year == 2014) ticker.stop();
+ year = d3.format('.1f')((+year) + 0.1);
 },tickDuration);
 
 });
 
 const halo = function(text, strokeWidth) {
 text.select(function() { return this.parentNode.insertBefore(this.cloneNode(true), this); })
- .style('fill', '#ffffff')
-  .style( 'stroke','#ffffff')
-  .style('stroke-width', strokeWidth)
-  .style('stroke-linejoin', 'round')
-  .style('opacity', 1);
+.style('fill', '#ffffff')
+ .style( 'stroke','#ffffff')
+ .style('stroke-width', strokeWidth)
+ .style('stroke-linejoin', 'round')
+ .style('opacity', 1);
 
 }
